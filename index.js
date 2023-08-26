ee330f80df396d9cb9bbfbf0377694911d92427f
kaustabhws <118001401+kaustabhws@users.noreply.github.com>
2023-08-26 13:41:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ app.use(cors())
 app.use(express.json())
 
 // Available Routes
-app.use('/', (req, res) => {
-  res.json({"message": "App deployed successfully"})
-})
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
